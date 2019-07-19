46952e7a5b8aa742bcb479bd37f3e40a23189796
Nikhil Chawla <nichawla@redhat.com>
2019-07-19 21:38:49 +0530

    Adding reusable redis PUBSUSB scripts

---
+++ redis_scripts/subscriber.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import time
+import traceback
+import sys
+import redis
+import argparse
+
+
+def run_subscriber(redis_host, redis_port, benchmark):
+    try:
+        r = redis.StrictRedis(host=redis_host, port=redis_port)
+
+        p = r.pubsub()
+        p.subscribe(benchmark)
+        STATE = True
+        count = 1
+
+        while STATE:
+            print("Waiting For all {} Pods to get ready ...{}".format(benchmark,count))
+            count += 1
+            message = p.get_message()
+            if message:
+                command = message['data']
+                if command == b'run':
+                    STATE = False
+
+            time.sleep(1)
+
+        print("Executing {}...".format(benchmark))
+        return True
+    except Exception as e:
+        print("******Exception Occured*******")
+        print(str(e))
+        print(traceback.format_exc())
+        return False
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='Redis Subscriber')
+    parser.add_argument("--redis-host", help="input the redis server address. DEFAULT: localhost", default="localhost", type=str)
+    parser.add_argument("--redis-port", help="input the redis port. DEFAULT: 6379", default=6379, type=int)
+    parser.add_argument("benchmark", help="input the benchmark to be executed", type=str)
+    args = parser.parse_args()
+    redis_host = args.redis_host
+    redis_port = args.redis_port
+    benchmark = args.benchmark
+    run_subscriber(redis_host, redis_port, benchmark)
+
+
+if __name__ == '__main__':
+    sys.exit(main())

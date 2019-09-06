92fb01599b2b1c0c21e38b43e7dca010c48770df
Dustin Black <dblack@redhat.com>
2019-09-06 15:46:47 -0400

    add scripts for dropping ceph caches

---
+++ ceph-cache-dropper/osd-cache-drop-websvc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import subprocess
+import logging
+import cherrypy
+
+stdout_log = open('/tmp/dropcache.log', 'w')
+
+logger = logging.getLogger('dropcache')
+
+class DropOSDCache(object):
+    @cherrypy.expose
+    #@cherrypy.tools.json_out()
+    #@cherrypy.tools.json_in()
+
+    def drop_osd_caches(self):
+        try:
+            result = subprocess.check_output(
+                ["/bin/sh", "/drop-osd-cache-within-toolbox.sh"])
+        except subprocess.CalledProcessError as e:
+            logger.error('failed to drop cache')
+            logger.exception(e)
+            return 'FAIL'
+        logger.info(result)
+        return 'SUCCESS'
+
+if __name__ == '__main__':
+    config = { 
+        'global': {
+            'server.socket_host': '0.0.0.0' ,
+            'server.socket_port': 9432,
+        },
+    }
+    cherrypy.config.update(config)
+    cherrypy.quickstart(DropOSDCache())

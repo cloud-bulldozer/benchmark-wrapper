b780a1a319988a0f45e90a1d4b6b4b6f39cbd590
Ben England <bengland@redhat.com>
2019-09-16 15:50:07 -0400

    Fs drift run snafu (#43)
    Add fs-drift benchmark to snafu

--- utils/wrapper_factory.py
+++ utils/wrapper_factory.py
@@ -4,11 +4,16 @@ from fio_wrapper.fio_wrapper import fio_wrapper
 from smallfile_wrapper.smallfile_wrapper import smallfile_wrapper
 #from pgbench_wrapper.pgbench_wrapper import pgbench_wrapper
 #from uperf_wrapper.uperf_wrapper import uperf_wrapper
+from fs_drift_wrapper.fs_drift_wrapper import fs_drift_wrapper
 
 import logging
 logger = logging.getLogger("snafu")
 
-wrapper_dict = {"fio": fio_wrapper, "smallfile": smallfile_wrapper}
+wrapper_dict = {
+    "fio": fio_wrapper,
+    "smallfile": smallfile_wrapper,
+    "fs-drift": fs_drift_wrapper
+}
 #    "backpack": pgbench_wrapper,
 #    "fio": fio_wrapper,
 #    "pgbench": pgbench_wrapper,

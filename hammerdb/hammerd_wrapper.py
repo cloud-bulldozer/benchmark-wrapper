66f69e541ac0d7bcf3c3935d4d167192a78c0082
Marko Karg <mkarg@redhat.com>
2019-12-18 09:41:15 +0100

    added missing, possibly useful metadata fields

--- hammerdb/hammerd_wrapper.py
+++ hammerdb/hammerd_wrapper.py
@@ -34,22 +34,24 @@ def _parse_stdout(stdout):
             data.append(entry)
     return data
 
-def _json_payload(data, iteration, uuid, db_server, db_port, db_warehouses, db_num_workers, transactions, test_type, runtime, rampup, samples):
+def _json_payload(data, uuid, db_server, db_port, db_warehouses, db_num_workers, db_tcp, db_user, transactions, test_type, runtime, rampup, samples, timed_test):
     processed = []
     for i in range(0,len(data)):
         processed.append({
             "workload" : "hammerdb",
             "uuid" : uuid,
-            "iteration": int(iteration),
             "db_server" : db_server,
             "db_port" : db_port,
             "db_warehouses" : db_warehouses,
             "db_num_workers" : db_num_workers,
+            "db_tcp": db_tcp,
+            "db_user": db_user,
             "transactions": transactions,
             "test_type": test_type,
             "runtime": runtime,
             "rampup": rampup,
             "samples": samples,
+            "timed_test": timed_test,
             "num_workers": (len(data) -1),
             "worker": data[i][0],
             "tpm": data[i][1],
@@ -62,7 +64,7 @@ def _summarize_data(data):
         entry = data[0]
 
         print("+{} HammerDB Results {}+".format("-"*(50), "-"*(50)))
-        print("Run : {}".format(entry['iteration']))
+        #print("Run : {}".format(entry['iteration']))
         print("HammerDB setup")
         #print("""
         #      server: {}""".format(entry['remote_ip']))
@@ -70,12 +72,17 @@ def _summarize_data(data):
         print("HammerDB results for:")
         print("Database server: {}".format(entry['db_server']))
         print("Database port: {}".format(entry['db_port']))
-        print("Database warehouses: {}".format(entry['db_warehouses']))
-        print("Database workers: {}".format(entry['db_num_workers']))
+        print("Number of database warehouses: {}".format(entry['db_warehouses']))
+        print("Number of workers: {}".format(entry['db_num_workers']))
+        print("TCP connection to the DB: {}".format(entry['db_tcp']))
+        print("Database user: {}".format(entry['db_user']))
         print("Transactions: {}".format(entry['transactions']))
+        print("Test type: {}".format(entry['test_type']))
+        print("Runtime: {}".format(entry['runtime']))
+        print("Rampup time: {}".format(entry['rampup']))
         print("Worker: {}".format(entry['worker']))
         print("Samples: {}".format(entry['samples']))
-        print("Test type: {}".format(entry['test_type']))
+        print("Timed test: {}".format(entry['timed_test']))
         #print("""
         #      test_type: {}
         #      protocol: {}
@@ -107,10 +114,9 @@ def main():
             help='Rampup time for the run')
     args = parser.parse_args()
 
-    #server = "bullwinkle-elk.rdu.openstack.engineering.redhat.com"
-    server = "marquez.perf.lab.eng.rdu2.redhat.com"
-    port = "9200"
-    protocol = "tcp"
+    server = ""
+    port = ""
+    protocol = ""
     uuid = ""
     db_user = ""
     db_server = ""
@@ -121,13 +127,15 @@ def main():
     runtime = ""
     rampup = ""
     samples = ""
-    iteration = "1" # needs to be changed, comes from the caller
-    test_type = "tpc-c"
+    iteration = "" 
+    test_type = ""
 
+    if "es_server" in os.environ:
+        es_server = os.environ["es_server"]
+    if "es_port" in os.environ:
+        es_port = os.environ["es_port"]
     if "uuid" in os.environ:
         uuid = os.environ["uuid"]
-    if "db_user" in os.environ:
-        db_user = os.environ["db_user"]
     if "db_server" in os.environ:
         db_server = os.environ["db_server"]
     if "db_port" in os.environ:
@@ -136,26 +144,34 @@ def main():
         db_warehouses = os.environ["db_warehouses"]
     if "db_num_workers" in os.environ:
         db_num_workers = os.environ["db_num_workers"]
+    if "db_tcp" in os.environ:
+        db_tcp = os.environ["db_tcp"]
+    if "db_user" in os.environ:
+        db_user = os.environ["db_user"]
     if "transactions" in os.environ:
         transactions = os.environ["transactions"]
+    if "test_type" in os.environ:
+        test_type = os.environ["test_type"]
     if "runtime" in os.environ:
         runtime = os.environ["runtime"]
     if "rampup" in os.environ:
         rampup = os.environ["rampup"]
     if "samples" in os.environ:
         samples = os.environ["samples"]
+    if "timed_test" in os.environ:
+        timed_test = os.environ["timed_test"]
 
 
-    stdout = _run_hammerdb()
-    #stdout = _fake_run()
+    #stdout = _run_hammerdb()
+    stdout = _fake_run()
     if stdout[1] == 1:
         print ("hammerdbcli failed to execute, trying one more time..")
-        stdout = _fake_run()
+        stdout = _run_hammerdb()
         if stdout[1] == 1:
             print ("hammerdbcli failed to execute a second time, stopping...")
             exit(1)
     data = _parse_stdout(stdout[0])
-    documents = _json_payload(data, iteration, uuid, db_server, db_port, db_warehouses, db_num_workers, transactions, test_type, runtime, rampup, samples)
+    documents = _json_payload(data, uuid, db_server, db_port, db_warehouses, db_num_workers, db_tcp, db_user, transactions, test_type, runtime, rampup, samples, timed_test)
     if server != "" :
         if len(documents) > 0 :
             _index_result("ripsaw-hammerdb-results",server,port,documents)

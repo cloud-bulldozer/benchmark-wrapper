b963d8753b3310f8448a5a741eebd7e388b978ef
Marko Karg <mkarg@redhat.com>
2020-01-28 15:45:12 +0100

    removed superfluous files

--- hammerdb/build_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-podman build --no-cache --tag=hammerdb .
-podman run localhost/hammerdb
-ID=$(podman ps --all | head -n 2 | grep -v CONTAINER | cut -d " " -f 1)
-echo $ID
-podman commit $ID quay.io/mkarg/hammerdb
-podman push quay.io/mkarg/hammerdb:latest

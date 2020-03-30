4947df2ca454a51abd3915446ff0f66ad882ad30
Amit Sagtani <asagtani06@gmail.com>
2020-03-31 00:17:01 +0530

    added ycsb to run from run_snafu --tool (#139)
    Migrated ycsb to now run through run_snafu

--- utils/wrapper_factory.py
+++ utils/wrapper_factory.py
@@ -6,6 +6,7 @@ from smallfile_wrapper.smallfile_wrapper import smallfile_wrapper
 # from uperf_wrapper.uperf_wrapper import uperf_wrapper
 from fs_drift_wrapper.fs_drift_wrapper import fs_drift_wrapper
 from cluster_loader.cluster_loader import cluster_loader_wrapper
+from ycsb_wrapper.ycsb_wrapper import ycsb_wrapper
 
 import logging
 logger = logging.getLogger("snafu")
@@ -14,7 +15,8 @@ wrapper_dict = {
     "fio": fio_wrapper,
     "smallfile": smallfile_wrapper,
     "fs-drift": fs_drift_wrapper,
-    "cl": cluster_loader_wrapper
+    "cl": cluster_loader_wrapper,
+    "ycsb": ycsb_wrapper
 }
 #    "backpack": pgbench_wrapper,
 #    "fio": fio_wrapper,

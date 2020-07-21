a403ca5a56e78ff58bea81771547a809bd721376
Ben England <bengland@redhat.com>
2020-07-21 09:54:50 -0400

    fix indentation problems associated with try block

--- utils/request_cache_drop.py
+++ utils/request_cache_drop.py
@@ -10,6 +10,9 @@ import http.client
 import sys
 import logging
 
+class RunSnafuCacheDropException(Exception):
+    pass
+
 dropKernelCachePort = 9435  # FIXME: hardcoded for now
 dropCephCachePort = 9437  # FIXME: hardcoded for now
 logger = logging.getLogger("snafu")
@@ -21,13 +24,29 @@ if dbgLevel != None:
 
 http_debug_level = int(os.getenv('HTTP_DEBUG_LEVEL', default=0))
 
-http_timeout = 2
+http_timeout = 10
 
 def drop_cache():
-    kernel_cache_drop_pod_ips = os.getenv('kcache_drop_pod_ips')
-    logger.info('kernel cache drop pods: %s' % str(kernel_cache_drop_pod_ips))
+
+    # drop Ceph OSD cache if requested to
+    
     ceph_cache_drop_pod_ip = os.getenv('ceph_drop_pod_ip')
     logger.info('ceph OSD cache drop pod: %s' % str(ceph_cache_drop_pod_ip))
+    if ceph_cache_drop_pod_ip != None:
+        conn = http.client.HTTPConnection(ceph_cache_drop_pod_ip, port=dropKernelCachePort, timeout=http_timeout)
+        if http_debug_level > 0:
+            conn.set_debuglevel(http_debug_level)
+        logger.info('requesting ceph to drop cache via %s:%d' % (ip, dropCephCachePort))
+        conn.request('GET', '/DropCephCache')
+        rsp = conn.getresponse()
+        if rsp.status != http_ok:
+            logger.error('HTTP ERROR %d: %s' % (rsp.status, rsp.reason))
+            raise RunSnafuCacheDropException('Ceph OSD cache drop %s:%d' % (ip, dropKernelCachePort))
+
+    # drop kernel cache if requested to
+
+    kernel_cache_drop_pod_ips = os.getenv('kcache_drop_pod_ips')
+    logger.info('kernel cache drop pods: %s' % str(kernel_cache_drop_pod_ips))
     if kernel_cache_drop_pod_ips != None:
         pod_ip_list = kernel_cache_drop_pod_ips.split()
         for ip in pod_ip_list:
@@ -35,40 +54,11 @@ def drop_cache():
             if http_debug_level > 0:
                 conn.set_debuglevel(http_debug_level)
             logger.info('requesting kernel to drop cache via %s:%d' % (ip, dropKernelCachePort))
-            try:
-                conn.request('GET', '/DropKernelCache')
-                rsp = conn.getresponse()
-                if rsp.status != http.client.OK:
-                    logger.error('HTTP code %d: %s' % (rsp.status, rsp.reason))
-                    raise RunSnafuCacheDropException(ip, dropKernelCachePort)
-            except http.client.HTTPException as e:
-                logger.error('failed to request kernel cache drop' % (rsp.status, rsp.reason))
-                logger.exception(e)
-                raise e
-            except TimeoutError as e:
-                logger.error('timeout requesting kernel cache drop')
-                logger.exception(e)
-                raise e
-
-    if ceph_cache_drop_pod_ip != None:
-        conn = http.client.HTTPConnection(ceph_cache_drop_pod_ip, port=dropKernelCachePort, timeout=http_timeout)
-        if http_debug_level > 0:
-            conn.set_debuglevel(http_debug_level)
-        logger.info('requesting ceph to drop cache via %s:%d' % (ip, dropCephCachePort))
-        try:
-            conn.request('GET', '/DropCephCache')
+            conn.request('GET', '/DropKernelCache')
             rsp = conn.getresponse()
-            if rsp.status != http_ok:
-                logger.error('HTTP ERROR %d: %s' % (rsp.status, rsp.reason))
-                raise RunSnafuCacheDropException('Ceph OSDs', ip, dropKernelCachePort)
-        except http.client.HTTPException as e:
-            logger.error('failed to request Ceph OSD cache drop' % (rsp.status, rsp.reason))
-            logger.exception(e)
-            raise e
-        except TimeoutError as e:
-            logger.error('timeout requesting kernel cache drop')
-            logger.exception(e)
-            raise e
-
+            if rsp.status != http.client.OK:
+                logger.error('HTTP code %d: %s' % (rsp.status, rsp.reason))
+                raise RunSnafuCacheDropException('kernel cache drop %s:%d' % (ip, dropKernelCachePort))
+ 
 if __name__ == '__main__':
     drop_cache()

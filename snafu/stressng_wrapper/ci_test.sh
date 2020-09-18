10a90087c406465523e480f8845155a2936d877f
Marko Karg <mkarg@redhat.com>
2020-09-18 13:53:35 +0200

    corrected the path name

--- snafu/stressng_wrapper/ci_test.sh
+++ snafu/stressng_wrapper/ci_test.sh
@@ -6,6 +6,6 @@ source ci/common.sh
 
 # Build image for ci
 image_spec=$SNAFU_WRAPPER_IMAGE_PREFIX/stressng:$SNAFU_IMAGE_TAG 
-build_and_push snafu/stressng/Dockerfile $image_spec
+build_and_push snafu/stressng_wrapper/Dockerfile $image_spec
 pushd ripsaw
 source tests/test_stressng.sh

43e958f4c0dab672d687f139f86b4e21dc672872
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2021-04-28 00:42:47 -0400

    Add DNS performance workload wrapper
    This commit adds a wrapper to run dns-perf which sends queries to
    the specified DNS server given a profile with the records in order
    to test the DNS performance. It parses the results and generated a
    json payload which can be indexed into Elasticsearch to help with
    the analysis.

---
+++ snafu/dns_perf_wrapper/dns_perf_wrapper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+import os
+import argparse
+from .trigger_dns_perf import Trigger_dns_perf
+
+
+class dns_perf_wrapper():
+    def __init__(self, parent_parser):
+        """ Initialize supported arguments """
+
+        parser_object = argparse.ArgumentParser(
+            description="DNS perf workload Wrapper script",
+            parents=[parent_parser],
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        )
+        parser = parser_object.add_argument_group("DNS Perf")
+        parser.add_argument("-u", "--uuid", required=True, help="Provide the uuid")
+        parser.add_argument("--server-address", required=True, help="DNS server address to send the requests")
+        parser.add_argument("--queries-per-second", required=True, type=int, help="Number of queries per second")
+        parser.add_argument("--run-time", type=int, required=True, help="run for at most this many seconds")
+        parser.add_argument(
+            "--data-file", required=True, help="File path with the records to send as queries to the server")
+        parser.add_argument("--clients", default=1, help="The number of clients to act as")
+        
+        self.args = parser_object.parse_args()
+
+        self.args.cluster_name = os.getenv("clustername", "mycluster")
+
+    def run(self):
+        yield Trigger_dns_perf(self.args)
--- snafu/utils/wrapper_factory.py
+++ snafu/utils/wrapper_factory.py
@@ -16,6 +16,7 @@ from snafu.trex_wrapper.trex_wrapper import trex_wrapper
 from snafu.flent_wrapper.flent_wrapper import flent_wrapper
 from snafu.log_generator_wrapper.log_generator_wrapper import log_generator_wrapper
 from snafu.image_pull_wrapper.image_pull_wrapper import image_pull_wrapper
+from snafu.dns_perf_wrapper.dns_perf_wrapper import dns_perf_wrapper
 
 import logging
 
@@ -40,6 +41,7 @@ wrapper_dict = {
     "flent": flent_wrapper,
     "log_generator": log_generator_wrapper,
     "image_pull": image_pull_wrapper,
+    "dns_perf": dns_perf_wrapper
 }
 
 

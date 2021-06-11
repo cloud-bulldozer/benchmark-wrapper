f9c23a26461eba804c2d50e452975b290295dd69
Ryan Drew <learnitall0@gmail.com>
2021-06-11 13:35:21 -0600

    Extend run_process with more options and check_file method

--- snafu/wrapper.py
+++ snafu/wrapper.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tools for wrapping external tools."""
+import os
 from typing import Any, Dict, Iterable, List, NewType, Set, Tuple
 from abc import ABC, abstractmethod
+import datetime
 import logging
 import subprocess
 import argparse
@@ -124,6 +126,35 @@ class Wrapper(ABC, metaclass=registry.ToolRegistryMeta):
                 return False
         return True
 
+    def check_file(self, file: str, perms: int = None) -> bool:
+        """
+        Check that the given file (relative or absolute dir) exists and has the given minimum permissions.
+        Given ``perms`` should be combination of ``os.R_OK``, ``os.W_OK`` and ``os.X_OK``. By default
+        will just check if the file is readable.
+
+        Returns
+        -------
+        bool
+            ``True`` if the file exists and has min perms, ``False`` otherwise
+
+        Examples
+        --------
+        >>> from snafu.wrapper import Wrapper
+        >>> class MyTool(Wrapper):
+        ...     tool_name = "my_tool"
+        ...
+        ...     def __init__(self):
+        ...         super().__init__()
+        >>> mytool = MyTool()
+        >>> mytool.check_file(os.path.join(__file__))
+        True
+        """
+
+        if perms is None:
+            perms = os.R_OK
+        perms |= os.F_OK
+        return os.access(os.path.abspath(file), perms)
+
     def preflight_checks(self) -> bool:
         """
         Preflight checks to run before setup. By default runs ``check_required_args``.
@@ -198,12 +229,38 @@ class Benchmark(Wrapper, ABC):
         super().__init__(**kwargs)
         self.metadata: List[str] = metadata if metadata is not None else list()
 
-    def run_process(self, cmd: str) -> Tuple[str, str, int]:
+    @staticmethod
+    def _run_process(cmd: str) -> Tuple[str, str, int]:
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        return stdout.strip().decode("utf-8"), stderr.strip().decode("utf-8"), process.returncode
+
+    def run_process(self, cmd: str, retries: int = 0, time: bool = False, expected_rc: int = 0) -> Dict:
         """
-        Run the given command as a subprocess within a shell and return stdout, stderr and returncode.
+        Run the given command as a subprocess within a shell and return information about the run within
+        a dict.
+
+        Arguments
+        ---------
+        cmd : str
+            Command to run. On each failed attempt, a dict containing metadata will be appended to the list
+            at 'failed' key. stdout, stderr and return code will be saved in "stdout", "stderr" and "rc"
+            keys respectively. On the successful attempt, each key will be stored in the returnd
+            dict itself. If stdout or stderr is empty, then it will not be returned.
+        retries : int, optional
+            Number of retries for running the command. Will run the command once, check return code, then
+            re-run ``retries`` number of times. Defaults to 0. For each attempt, attempt number will be
+            stored in "attempt" key and expected return code will be stored in "expected_rc".
+        time : bool, optional
+            If True, will time execution and return number of seconds it command took to run.
+            Defaults to False. Run time will be saved in the "elapsed_seconds" key for each attempt.
+        expected_rc : int, optional
+            Expected return code of command. Defaults to 0.
 
         Examples
         --------
+        >>> from pprint import pprint # for doctest
+        >>> import random
         >>> from snafu.wrapper import Benchmark
         >>> class MyBenchmark(Benchmark):
         ...     tool_name = "mybenchmark"
@@ -214,14 +271,69 @@ class Benchmark(Wrapper, ABC):
         ...     def emit_metrics(self):
         ...         yield dict()
         >>> mybench = MyBenchmark()
-        >>> mybench.run_process("echo 'Hello World'")
-        ('Hello World', '', 0)
+        >>> pprint(mybench.run_process("echo 'Hello World'"))
+        {'attempt': 1, 'expected_rc': 0, 'rc': 0, 'stdout': 'Hello World'}
+        >>> int(mybench.run_process("sleep 1", time=True)['elapsed_seconds'])
+        1
+        >>> pprint(mybench.run_process("echo 'test' | grep 'hello'", expected_rc=1))
+        {'attempt': 1, 'expected_rc': 1, 'rc': 1}
+        >>> fn = random.randint(1, 10)
+        >>> pprint(mybench.run_process(f"echo -n 'test' >> /tmp/{fn}; grep -w testtest /tmp/{fn}", retries=1))
+        {'attempt': 2,
+         'expected_rc': 0,
+         'failed': [{'attempt': 1, 'expected_rc': 0, 'rc': 1}],
+         'rc': 0,
+         'stdout': 'testtest'}
+        >>> pprint(mybench.run_process(f"rm /tmp/{fn}"))
+        {'attempt': 1, 'expected_rc': 0, 'rc': 0}
         """
 
         self.logger.info(f"Running command {cmd}")
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        return stdout.strip().decode("utf-8"), stderr.strip().decode("utf-8"), process.returncode
+        result: Dict[str, Any] = dict()
+
+        tries: int = 0
+
+        while tries <= retries:
+            tries += 1
+            self.logger.debug(f"On try {tries}")
+            attempt: Dict[str, Any] = dict()
+
+            if time:
+                start_time = datetime.datetime.utcnow()
+
+            stdout, stderr, rc = self._run_process(cmd)
+
+            if time:
+                end_time = datetime.datetime.utcnow()
+                diff_seconds = (end_time - start_time).total_seconds()
+                self.logger.debug(f"Run time: {diff_seconds} seconds")
+
+            # only add if not empty
+            for key, val in [("stdout", stdout), ("stderr", stderr)]:
+                if len(val) > 0:
+                    attempt[key] = val
+            attempt["rc"] = rc
+            attempt["expected_rc"] = expected_rc
+            attempt["attempt"] = tries
+            if time:
+                attempt["elapsed_seconds"] = diff_seconds
+
+            self.logger.debug(f"Got return code {rc}, expected {expected_rc}")
+            if rc == expected_rc:
+                self.logger.info(f"Command successful!")
+                result.update(attempt)
+                break
+            else:
+                self.logger.info(f"Command unsuccessful")
+                if result.get("failed", None) is None:
+                    result["failed"] = [attempt]
+                else:
+                    result["failed"].append(attempt)
+        else:
+            # If we hit retry limit, we go here
+            self.logger.info(f"Tried running command {tries} times, with no success.")
+
+        return result
 
     @abstractmethod
     def run(self) -> None:

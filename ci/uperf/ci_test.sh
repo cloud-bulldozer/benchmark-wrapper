77c6090bc3f89645f040884492ef70b9a01eb2b9
Ryan Drew <learnitall0@gmail.com>
2021-06-16 11:34:39 -0600

    Move uperf CI files to ci dir
    New proposed benchmark format has a single module for each benchmark,
    meaning now we need a new place to hold CI files. This moves those
    files into the CI dir. Helps with pythonic structure as well, as
    now we are not storing data files in the Python module source code.

--- ci/uperf/ci_test.sh
+++ ci/uperf/ci_test.sh
@@ -6,6 +6,6 @@ source ci/common.sh
 
 # Build image for ci
 image_spec=$SNAFU_WRAPPER_IMAGE_PREFIX/uperf:$SNAFU_IMAGE_TAG
-build_and_push snafu/uperf_wrapper/Dockerfile $image_spec
+build_and_push ci/uperf/Dockerfile $image_spec
 pushd ripsaw
 source tests/test_uperf.sh

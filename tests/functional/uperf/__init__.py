83ccaba9ceb6f35ab909cfb2729a57323dcaa6e2
Ryan Drew <learnitall0@gmail.com>
2021-07-09 13:55:05 -0600

    Add initial functional test structure for uperf

---
+++ tests/functional/uperf/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Functional tests for the uperf benchmark."""
---
+++ tests/functional/uperf/test_uperf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Perform functional tests of uperf."""
+import shlex
+import subprocess
+
+
+def test_basic_run_of_uperf(compose):
+    """
+    Test that we can run a command within the 'uperf' container.
+
+    If this test fails then either the compose file has changed or something went wrong starting
+    the container.
+    """
+
+    result = subprocess.run(
+        shlex.split(compose + " exec uperf echo 'Hello World'"),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+    )
+    result_stdout = result.stdout.decode("utf-8")
+    assert "Hello World" in result_stdout

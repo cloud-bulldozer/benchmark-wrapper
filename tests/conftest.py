61f9477a7eb0ba1784cd1a4afa114145225ec471
Ryan Drew <learnitall0@gmail.com>
2021-07-09 13:51:31 -0600

    Modify compose fixture to return base command

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,15 +18,15 @@ def get_compose_file(request):
     Returns
     -------
     tuple or None
-        First item is the path to the directory that the compose file was found in, second
-        is the path to the compose file. If a compose file was not found, then ``None`` is returned.
+        First item is the path to the directory that the test is located within. The second item
+        is the path to the compose file, if one was found that is readable, otherwise ``None``.
     """
 
     package_directory = os.path.dirname(request.fspath)
     compose_file = os.path.join(package_directory, COMPOSE_FILENAME)
     if os.access(compose_file, os.F_OK | os.R_OK):
         return package_directory, compose_file
-    return None
+    return package_directory, None
 
 
 @pytest.fixture(scope="package")
@@ -34,17 +34,28 @@ def compose(get_compose_file):  # pylint: disable=W0621
     """
     Run the compose file found within test's package and cleanup after all tests in package finish.
 
+    Raises
+    ------
+    RuntimeError
+        If an error occurs while trying to start or stop docker-compose.
+
     Returns
     -------
-    None
+    str
+        Base docker-compose command that can be used by tests in order to exec into containers.
+        For instance the string "docker-compose --file ... --project-directory ..." could be returned,
+        which a test can then append " exec container_name echo 'hello world'" to, in order to run an echo
+        command within the container with the name "container_name".
     """
 
     project_dir, compose_file = get_compose_file
-    if get_compose_file is None:
-        return
+    if compose_file is None:
+        raise RuntimeError(
+            f"Unable to find compose file within directory the test was collected from: {project_dir}"
+        )
 
     base_cmd = f"docker-compose --file {compose_file} --project-directory {project_dir}"
-    start_cmd = shlex.split(f"{base_cmd} up")
+    start_cmd = shlex.split(f"{base_cmd} up -d")
     try:
         subprocess.run(start_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
     except subprocess.CalledProcessError as proc_error:
@@ -52,7 +63,7 @@ def compose(get_compose_file):  # pylint: disable=W0621
             f"Unable to start compose file {compose_file}: {proc_error} Output: {proc_error.stdout}"
         ) from proc_error
 
-    yield
+    yield base_cmd
 
     end_cmd = shlex.split(f"{base_cmd} down --volumes")
     try:

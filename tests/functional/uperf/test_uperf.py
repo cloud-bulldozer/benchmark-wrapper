101ec81ce9ac6c713951aa2c28a2915c270a2d0e
Ryan Drew <learnitall0@gmail.com>
2021-07-16 07:34:26 -0600

    Add uperf_server container to test_uperf_is_available test

--- tests/functional/uperf/test_uperf.py
+++ tests/functional/uperf/test_uperf.py
@@ -8,25 +8,27 @@ import subprocess
 POD = "uperf-test-pod"  # pod prefix where containers are placed
 ES = "es01"  # container name for elasticsearch
 CLIENT = "uperf"  # container name for uperf client
+SERVER = "uperf_server"  # container name for uperf server
 TIMEOUT = 60  # universal timeout value we can use while waiting for containers to be ready
 
 
 def test_uperf_is_available(manifest):
     """
-    Test that we can run a command within the 'uperf' container.
+    Test that we can run a command within the 'uperf' and 'uperf_server' container.
 
     If this test fails then either the manifest file has changed or something went wrong starting
     the container.
     """
 
-    result = subprocess.run(
-        shlex.split(f"{manifest.format(container=f'{POD}-{CLIENT}')} echo 'Hello World'"),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-    )
-    result_stdout = result.stdout.decode("utf-8")
-    assert "Hello World" in result_stdout
+    for container in (CLIENT, SERVER):
+        result = subprocess.run(
+            shlex.split(f"{manifest.format(container=f'{POD}-{container}')} echo 'Hello World'"),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+        result_stdout = result.stdout.decode("utf-8")
+        assert "Hello World" in result_stdout
 
 
 def test_es_is_available(manifest, command_poll):

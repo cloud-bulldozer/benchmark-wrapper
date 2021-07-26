4769b0819128855672f887fdd26a294e8517d0b8
Ryan Drew <learnitall0@gmail.com>
2021-07-26 10:04:48 -0600

    Modify sphinx build to re-run notebooks on build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,6 @@ html_theme_options = {"collapse_navigation": False}  # show + next to items in T
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- nbsphinx configuration --------------------------------------------------
+nbsphinx_execute = "always"

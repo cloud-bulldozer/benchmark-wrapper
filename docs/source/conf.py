7aa3ae82d987b321e63b8b02b981d96ca23780b5
Ryan Drew <learnitall0@gmail.com>
2021-07-26 10:02:26 -0600

    Suppress mathjax warning from myst-parser plugin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = "0.0.1"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["myst_parser", "nbsphinx"]
+suppress_warnings = ["myst.mathjax"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

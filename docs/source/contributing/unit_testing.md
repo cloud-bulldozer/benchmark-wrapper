ac3ccf862c6f72700da65f342a45d9282b2903a9
Ryan Drew <rydrew@redhat.com>
2021-08-09 07:53:50 -0600

    Add Unit and Documentation Build Tests (#306)
    * Add test dependencies to setup.cfg
    
    * Add tox configuration
    
    * Add unit tests for process module while fixing various bugs
    
    * Add unit tests for config module while fixing various bugs
    
    * Add testing caches to gitgnore
    
    * Add unit testing GHA workflow
    
    * Add documentation test workflow
    
    * Suppress mathjax warning from myst-parser plugin
    
    * Add documentation page on testing practices
    
    * Modify sphinx build to re-run notebooks on build
    
    * Modify adding_workloads notebook to run on Azure
    
    * Remove needed ok-to-test label, as no need
    
    * Add manifest to force including version.txt
    
    * Add missing coverage configuration to tox
    
    * Remove '_source' directory from html_static_path
    
    * Add information on docstring tests
    
    * Remove extra space
    
    * Add export for benchmark operator image for ripsaw cli
    
    * Add coveragerc file
    
    * Add python 3.9 to doc and unit tests
    
    * Use matrix for parallel runs
    
    * Add missing needs to coverage_report job
    
    * Fix issues preventing coverage report job from running
    
    * Replace uppercase P with lowercase p in pip command
    
    * Add retry and timeout to linkcheck build
    
    * Increase timeout and retries for linkcheck
    
    * Change links in unit testing page from SFT to guru99
    
    * Add pretty name for jobs
    
    * Don't use coverage combine across python versions
    
    Not needed, as we have same tests for each python version
    
    We expect that the coverage will remain the same regardless of the python version
    
    * Only upload coverage report for python 3.6
    
    * Add missing install for coverage package
    
    * Use tox coverage environment to generate reports

---
+++ docs/source/contributing/unit_testing.md
@@ -0,0 +1,38 @@
+# Testing
+
+benchmark-wrapper uses [tox](https://pypi.org/project/tox/) and [pytest](https://docs.pytest.org/en/6.2.x/) for unit testing as well as documentation build testing.
+
+As a quick reminder, unit testing is defined as follows by [guru99.com](https://www.guru99.com/unit-testing-guide.html):
+
+> UNIT TESTING is a type of software testing where individual units or components of a software are tested. The purpose is to validate that each unit of the software code performs as expected.
+
+The goal for unit testing within benchmark-wrapper specifically is to ensure that all shared units (common modules and functionality) behave as expected, in order to create a solid foundation that all benchmarks may be based upon.
+
+For documentation build testing, the goal is to ensure that the documentation can build without errors and that there are no broken external links.
+
+Here are the main takeaways:
+
+* To write unit tests for a module, place them in ``tests/unit/test_<module>.py`` or within docstrings for small, basic functions.
+* Use Tox to invoke pytest unit tests and documentation build tests, by choosing from the following environments: ``py{36,37,38,39}-{unit,docs}``.
+* Use coverage reports to ensure thorough code testing.
+
+## Tox Usage
+
+There are eight distinct environments that Tox is configured for (through the ``tox.ini`` file within the project root), which is a permutation across four versions of Python and our two testing goals. These environments can be invoked by picking values from the following sets:
+
+``tox -e py{36,37,38,39}-{unit,docs}``
+
+For instance, using ``py36-unit`` will run unit tests for Python 3.6, while ``py38-docs`` will run a documentation build test for Python 3.8.
+
+
+## Writing Unit Tests
+
+Unit tests are placed under the ``tests/unit`` directory, container within individual Python modules. To keep a consistent structure, each module under this directory should correspond to one module within benchmark-wrapper. As an example, to create unit tests for ``snafu.module``, place them within ``tests/unit/test_module.py``.
+
+Tests can also live within docstrings, which will be invoked using pytest's [doctest functionality](https://docs.pytest.org/en/6.2.x/doctest.html). Docstring unit tests should be reserved for small, simple functions and/or to demonstrate example usage for users. This allows for automated regression testing and breaking change detection, as, if the example fails, then there must have been some change which will impact how the code is used. For numpy-style docstrings, docstring tests should live under the ["Examples"](https://numpydoc.readthedocs.io/en/latest/format.html#examples) section.
+
+For more information on how pytest can be leveraged to write unit tests, please check read the [pytest documentation](https://docs.pytest.org/en/6.2.x/example/index.html).
+
+## Code Coverage
+
+When unit tests are invoked using tox, [pytest-cov](https://pytest-cov.readthedocs.io/en/latest/readme.html) will be used to generate a coverage report, showing which lines were covered. Additionally, a [coverage file](https://coverage.readthedocs.io/en/coverage-5.5/) will be placed in the project root with the tox environment used as the file extension (i.e. ``.coverage.py{36,37,38,39}-unit``). Please use these coverage resources to help write unit tests for your PRs as needed. Note that code for benchmark wrappers are not included in these coverage reports, as benchmarks will be tested with [functional tests](https://www.guru99.com/functional-testing.html), rather than unit tests.
---
+++ tests/unit/test_config.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Test functionality in the config module."""
+import os
+import stat
+
+import configargparse
+
+import snafu.config
+
+
+def test_check_file_returns_bool_as_expected(tmpdir):
+    """Test that the check_file function will return ``True`` and ``False`` when it is supposed to."""
+
+    tmpdir.chmod(stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC)
+    test_file = tmpdir.join("testfile.txt")
+    test_file.write("some content")
+    test_file_path = test_file.realpath()
+    assert test_file.check()
+
+    test_perms = (
+        (os.R_OK, stat.S_IREAD),
+        (os.R_OK | os.W_OK, stat.S_IREAD | stat.S_IWRITE),
+        (os.R_OK | os.W_OK | os.EX_OK, stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC),
+    )
+
+    with tmpdir.as_cwd():
+        for check, perm in test_perms:
+            test_file.chmod(0)
+            assert snafu.config.check_file(test_file_path, perms=check) is False
+            test_file.chmod(perm)
+            assert snafu.config.check_file(test_file_path, perms=check) is True
+
+
+def test_none_or_type_function():
+    """Test that the none_or_type function returns function that casts values only when they aren't None."""
+
+    tests = (
+        (int, 1),
+        (int, "1"),
+        (str, "hey"),
+        (str, 2),
+        (dict, (("a", 1), ("b", 2))),
+    )
+
+    for expected_type, value in tests:
+        wrapped = snafu.config.none_or_type(expected_type)
+        assert wrapped(None) is None
+        assert wrapped(value) == expected_type(value)
+        assert isinstance(wrapped(value), expected_type)
+
+
+class TestConfig:
+    """Test functionality of Config class."""
+
+    test_args = (
+        snafu.config.ConfigArgument("param1", type=int),
+        snafu.config.ConfigArgument("param2", type=str),
+        snafu.config.ConfigArgument("--param3", type=str, env_var="param3"),
+        snafu.config.ConfigArgument("--param4", type=int, env_var="p4"),
+    )
+    test_input = ["1234", "my_string", "--param3", "1234", "--param4", "4321"]
+
+    @staticmethod
+    def verify_args(config: snafu.config.Config):
+        """Verify that config contains expected args from cls.test_args and cls.test_input."""
+
+        assert config.param1 == 1234
+        assert config.param2 == "my_string"
+        assert config.param3 == "1234"
+        assert config.param4 == 4321
+
+    @staticmethod
+    def get_config_instance() -> snafu.config.Config:
+        """Return a new Config instance with tool_name set to 'TEST'."""
+        try:
+            configargparse.init_argument_parser()
+        except ValueError:
+            del configargparse._parsers["default"]  # pylint: disable=W0212
+            configargparse.init_argument_parser()
+        return snafu.config.Config("TEST")
+
+    def test_init_creates_argparser(self):
+        """
+        Test that init creates an instance of ``configargparse.ArgParser``.
+
+        If this changes, then this test suite and benchmark options will need to be updated, as it's
+        expected that arguments to ``add_argument`` will change.
+        """
+
+        config = self.get_config_instance()
+        assert isinstance(config.parser, configargparse.ArgumentParser)
+
+    def test_can_add_args_and_parse_using_add_argument_method(self):
+        """Test that we can populate a parser and parse args using the ``add_argument`` method."""
+
+        config = self.get_config_instance()
+        for arg in self.test_args:
+            config.add_argument(*arg.args, **arg.kwargs)
+        config.parse_args(self.test_input)
+        self.verify_args(config)
+
+    def test_can_add_and_parse_using_populate_parser_method(self):
+        """Test that we can populate a parser and parse args using the ``populate_parser`` method."""
+
+        config = self.get_config_instance()
+        config.populate_parser(self.test_args)
+        config.parse_args(self.test_input)
+        self.verify_args(config)
+
+    def test_can_get_env_param_mapping(self):
+        """Test that env_to_params attribute becomes populated appropriately as we add arguments."""
+
+        config = self.get_config_instance()
+        assert config.env_to_params == {}
+        config.populate_parser(self.test_args)
+        assert config.env_to_params == {"param3": "param3", "p4": "param4"}
+
+    def test_get_env_returns_correct_env_mappings(self):
+        """Tests that the env_to_params attribute will return the correct env variable mappings."""
+
+        config = self.get_config_instance()
+        config.populate_parser(self.test_args)
+        assert config.get_env() == dict(os.environ)
+
+        config.parse_args(self.test_input)
+        self.verify_args(config)
+
+        env = config.get_env()
+        assert env["param3"] == str(config.param3)
+        assert env["p4"] == str(config.param4)
+        for key, val in os.environ.items():
+            assert env[key] == val

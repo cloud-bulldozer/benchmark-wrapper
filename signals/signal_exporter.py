09d7d9b00b077f715b9634845bb597151c1991db
maxusmusti <meyceoz@redhat.com>
2021-08-09 23:57:28 -0400

    Added module-level description

--- signals/signal_exporter.py
+++ signals/signal_exporter.py
@@ -9,6 +9,23 @@ import uuid
 import logging
 
 
+"""
+State/Event Signal Module
+
+Adds two new, simple-to-use objects:
+ - SignalExporter      (for publishing state signals and handling subscribers + responses)
+ - SignalResponder     (for receiving state signals, locking onto publishers, and publishing responses)
+
+Also provides two dataclass specifications:
+ - Signal              (state signal protocol definition)
+ - Response            (response protocol definition)
+
+Combining redis pubsub features with state signal + response protocols, 
+these additions make state signal publishing, subscribing, receiving, 
+and responding incredibly easy to integrate into any code.
+"""
+
+
 def _create_logger(
     class_name: str, process_name: str, log_level: str
 ) -> logging.Logger:

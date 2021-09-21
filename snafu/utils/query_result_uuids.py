039fe5ef189243e6f0d19e190cb367f7621806fe
Ben England <bengland@redhat.com>
2021-09-21 15:27:53 -0400

    move elasticsearch document retrieval inside func
    eliminates lots of global vars
    remove more dead code

--- snafu/utils/query_result_uuids.py
+++ snafu/utils/query_result_uuids.py
@@ -59,74 +59,64 @@ def compute_query():
 
 es = snafu.utils.fetch_es_test_results.connect_es()
 
-# used by strptime()
-datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
-
-# dictionary of all tests found
-uuid_table = {}
-
-debug = os.getenv("DEBUG")
-hits_so_far = 0
-skipped = 0
-index_time_query = compute_query()
-res = es.search(index=index_name, scroll="60s", size=1000, body=index_time_query)
-result_count = res["hits"]["total"]["value"]
-if debug:
-    print("Got %d Hits:" % result_count)
-scroll_id = res["_scroll_id"]
-while len(res["hits"]["hits"]) > 0:
+def get_uuid_tuples():
+    # dictionary of all tests found
+    uuid_table_out = {}
+    # used by strptime()
+    datetime_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+    debug = os.getenv("DEBUG")
+    hits_so_far = 0
+    index_time_query = compute_query()
+    res = es.search(index=index_name, scroll="60s", size=1000, body=index_time_query)
+    result_count = res["hits"]["total"]["value"]
     if debug:
-        print("")
-        print("scroll id %s" % scroll_id)
-    hit_list = res["hits"]["hits"]
-    hits_so_far += len(hit_list)
-    if debug:
-        sys.stdout.write("%d " % hits_so_far)
-    sys.stdout.flush()
-    for hit in hit_list:
-        mydoc = hit["_source"]
-        uuid = mydoc["uuid"]
-        cluster_name = mydoc["cluster_name"]
-        user = mydoc["user"]
-        try:
-            timestamp_field_value = mydoc[timestamp_fieldname]
-        except KeyError:
-            # hack to work around change in schema for fio results
-            # since this is in seconds, convert to millisec expected below
-            timestamp_field_value = mydoc["end_time"] * 1000.0
-        try:
-            timestamp = datetime.strptime(timestamp_field_value, datetime_format)
-        except TypeError:
-            timestamp_float_sec = timestamp_field_value / 1000.0
-            timestamp = datetime.fromtimestamp(timestamp_float_sec, tz=timezone.utc)
-        try:
-            update = False
-            (start_time, end_time, found_cname, found_user) = uuid_table[uuid]
-            assert cluster_name == found_cname
-            assert user == found_user
-            if start_time > timestamp:
+        print("Got %d Hits:" % result_count)
+    scroll_id = res["_scroll_id"]
+    while len(res["hits"]["hits"]) > 0:
+        if debug:
+            print("")
+            print("scroll id %s" % scroll_id)
+        hit_list = res["hits"]["hits"]
+        hits_so_far += len(hit_list)
+        if debug:
+            sys.stdout.write("%d " % hits_so_far)
+        sys.stdout.flush()
+        for hit in hit_list:
+            mydoc = hit["_source"]
+            uuid = mydoc["uuid"]
+            cluster_name = mydoc["cluster_name"]
+            user = mydoc["user"]
+            try:
+                timestamp_field_value = mydoc[timestamp_fieldname]
+            except KeyError:
+                # hack to work around change in schema for fio results
+                # since this is in seconds, convert to millisec expected below
+                timestamp_field_value = mydoc["end_time"] * 1000.0
+            try:
+                timestamp = datetime.strptime(timestamp_field_value, datetime_format)
+            except TypeError:
+                timestamp_float_sec = timestamp_field_value / 1000.0
+                timestamp = datetime.fromtimestamp(timestamp_float_sec, tz=timezone.utc)
+            try:
+                update = False
+                (start_time, end_time, found_cname, found_user) = uuid_table_out[uuid]
+                assert cluster_name == found_cname
+                assert user == found_user
+                if start_time > timestamp:
+                    start_time = timestamp
+                    update = True
+                if end_time < timestamp:
+                    end_time = timestamp
+                    update = True
+                if update:
+                    uuid_table_out[uuid] = (start_time, end_time, found_cname, found_user)
+            except KeyError:
                 start_time = timestamp
-                update = True
-            if end_time < timestamp:
                 end_time = timestamp
-                update = True
-            if update:
-                uuid_table[uuid] = (start_time, end_time, found_cname, found_user)
-        except KeyError:
-            start_time = timestamp
-            end_time = timestamp
-            uuid_table[uuid] = (start_time, end_time, cluster_name, user)
-
-    res = es.scroll(scroll_id=scroll_id, scroll="60s")
-print("")
-print("")
-
-# print out uuid time range
-
-unsorted_list = []
-for u in uuid_table.keys():
-    (start_time, end_time, found_cname, found_user) = uuid_table[u]
-    unsorted_list.append((start_time, end_time, u, found_cname, found_user))
+                uuid_table_out[uuid] = (start_time, end_time, cluster_name, user)
+    
+        res = es.scroll(scroll_id=scroll_id, scroll="60s")
+    return uuid_table_out
 
 
 def extract_timestamp(tpl):
@@ -134,10 +124,20 @@ def extract_timestamp(tpl):
     return start_time
 
 
+print("")
+print("")
+
+uuid_table = get_uuid_tuples()
+
+# sort uuids by start_time of results for that uuid
+
+unsorted_list = []
+for u in uuid_table.keys():
+    (start_time, end_time, found_cname, found_user) = uuid_table[u]
+    unsorted_list.append((start_time, end_time, u, found_cname, found_user))
 sorted_list = sorted(unsorted_list, key=extract_timestamp)
+
 for el in sorted_list:
     (start_time, end_time, u, found_cname, found_user) = el
     print("[ {}, {} ] {} {} {}".format(start_time, end_time, u, found_cname, found_user))
 
-if skipped > 0:
-    print("skipped %d out of %d results" % (skipped, result_count))

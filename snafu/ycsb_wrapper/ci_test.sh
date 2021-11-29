b411745f32e8a15ba512e3a865b6f1801a70eea4
Vicente Zepeda Mas <vzepedam@redhat.com>
2021-11-29 15:45:29 +0100

    Update ycsb image name
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- snafu/ycsb_wrapper/ci_test.sh
+++ snafu/ycsb_wrapper/ci_test.sh
@@ -5,7 +5,7 @@ set -x
 source ci/common.sh
 
 # Build image for ci
-image_spec=$SNAFU_WRAPPER_IMAGE_PREFIX/ycsb-server:$SNAFU_IMAGE_TAG
+image_spec=$SNAFU_WRAPPER_IMAGE_PREFIX/ycsb:$SNAFU_IMAGE_TAG
 build_and_push snafu/ycsb_wrapper/Dockerfile $image_spec
 pushd ripsaw
 source tests/test_ycsb.sh

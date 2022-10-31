e49cd23451f286841a69dec8532daed61eaa9876
Edwin Chong <8726446-ewchong@users.noreply.gitlab.com>
2022-10-31 10:57:18 -0400

    allow sysbench tool to take arguments
    The wrapper for sysbench only took a file for arguments. Added
    the ability to specify arguments through CLI using --args=""
    and arugements msut be surrounded by quotes

--- snafu/sysbench_wrapper/trigger_sysbench.py
+++ snafu/sysbench_wrapper/trigger_sysbench.py
@@ -10,24 +10,31 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+"""
+sysbench benchmark runs
+"""
 
 import logging
 import os
 import re
 import subprocess
+import sys
 from datetime import datetime
 
 logger = logging.getLogger("snafu")
 
 
 class trigger_sysbench:
-    def __init__(self, uuid, user, cluster_name, sysbench_file, sample):
+    """Logic for all sysbench runs"""
+
+    def __init__(self, uuid, user, cluster_name, sysbench_file, sample, sysbench_args):
 
         self.uuid = uuid
         self.user = user
         self.cluster_name = cluster_name
         self.sysbench_file = sysbench_file
         self.sample = sample
+        self.sysbench_args = sysbench_args
 
         #  blank dictionary to capture test configuration
         self.test_config = {}
@@ -40,12 +47,18 @@ class trigger_sysbench:
         specific sysbench versions/future changes.
         """
 
-        #  open config file
-        config_file = open(self.sysbench_file)
+        #  Use args from command line or a config file
+        if self.sysbench_args:
+            config_options = self.sysbench_args.split()
+        elif self.sysbench_file:
+            config_options = open(self.sysbench_file, "r", encoding="utf8")
+        else:
+            logger.error("Arugments were not specified by file or command line")
+            sys.exit(1)
 
         cmd = "sysbench"
         #  for each option add it to the command line
-        for option in config_file:
+        for option in config_options:
             option = option.strip()
             #  need to use --test for ease of use when parsing, but will strip off before adding to cmd
             if "--test" in option:

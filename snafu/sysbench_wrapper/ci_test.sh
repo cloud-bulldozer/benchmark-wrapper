c3d62e5d4daaf001a153af912c663cddc5bc3ded
Edwin Chong <8726446-ewchong@users.noreply.gitlab.com>
2022-11-02 09:48:07 -0400

    rename sysbench folder to adhere to naming standard
    The sysbench folder lacked the wrapper suffix causing it to get
    snagged in pylint

--- snafu/sysbench_wrapper/ci_test.sh
+++ snafu/sysbench_wrapper/ci_test.sh
@@ -6,6 +6,6 @@ source ci/common.sh
 
 # Build image for ci
 image_spec=$SNAFU_WRAPPER_IMAGE_PREFIX/sysbench:$SNAFU_IMAGE_TAG
-build_and_push snafu/sysbench/Dockerfile $image_spec
+build_and_push snafu/sysbench_wrapper/Dockerfile $image_spec
 pushd ripsaw
 source tests/test_sysbench.sh
